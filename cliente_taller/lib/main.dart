e5489216b9cac6de021edea41f6ff4eb81d98a96
LissetW <norlisset@comunidad.unam.mx>
2022-01-06 17:20:25 -0600

    conectando la bd

--- cliente_taller/lib/main.dart
+++ cliente_taller/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cliente_taller/Wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Container(child: Text('falló la conexión'));
+            // return SomethingWentWrong();
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(home: Wrapper());
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Container(child: Text('Cargando ...'));
+          // return Loading();
+        });
   }
 }
